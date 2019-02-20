d4d208893b8507e24d62a9f2bc85d1e424bc562e
Alexander Quiceno <yonnyquiceno@gmail.com>
2019-02-20 13:14:29 -0500

    Use unobtrusive AJAX calls to avoid page refreshing when voting

--- app/controllers/simple_discussion/forum_votes_controller.rb
+++ app/controllers/simple_discussion/forum_votes_controller.rb
@@ -2,16 +2,15 @@ class SimpleDiscussion::ForumVotesController < SimpleDiscussion::ApplicationCont
   CLASS_WHITELIST = ['ForumThread', 'ForumPost']
   before_action :authenticate_user!
   before_action :set_votable
-  before_action :set_redirect_path
 
   def create
     @votable.forum_votes.create(user: current_user)
-    redirect_to @redirect_path
+    respond_to { |format| format.js }
   end
 
   def destroy
     @votable.forum_votes.where(user: current_user).last.try(:destroy)
-    redirect_to @redirect_path
+    respond_to { |format| format.js }
   end
 
   private
@@ -19,13 +18,5 @@ class SimpleDiscussion::ForumVotesController < SimpleDiscussion::ApplicationCont
     def set_votable
       return unless CLASS_WHITELIST.include?(params[:votable_type])
       @votable = params[:votable_type].safe_constantize.find_by(id: params[:votable_id])
-    end
-
-    def set_redirect_path
-      @redirect_path = if params[:votable_type] == 'ForumThread'
-        @redirect_path = simple_discussion.root_path
-      elsif params[:votable_type] == 'ForumPost'
-        @redirect_path = simple_discussion.forum_thread_path(@votable.forum_thread)
-      end
     end
 end
