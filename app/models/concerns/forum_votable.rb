9475c216b1260d24e6d4a5a405c12cd9907238d8
Alexander Quiceno <yonnyquiceno@gmail.com>
2019-02-27 14:58:34 -0500

    Add action to remove forum threads and forum posts, add forum_votable_count counter cache colum to properlly sort threads and posts by forum_votable_count

--- app/models/concerns/forum_votable.rb
+++ app/models/concerns/forum_votable.rb
@@ -3,6 +3,8 @@ module ForumVotable
 
   included do
     has_many :forum_votes, as: :forum_votable
+
+    scope :most_voted_first, -> { order('forum_votable_count DESC') }
   end
 
   def voted_by?(user)
--- app/models/forum_vote.rb
+++ app/models/forum_vote.rb
@@ -1,5 +1,5 @@
 class ForumVote < ApplicationRecord
   belongs_to :user
-  belongs_to :forum_votable, polymorphic: true
+  belongs_to :forum_votable, polymorphic: true, counter_cache: :forum_votable_count
   validates :user_id, uniqueness: { scope: [:forum_votable_type, :forum_votable_id], message: "only one vote per user is allowed for each votable" }
 end
