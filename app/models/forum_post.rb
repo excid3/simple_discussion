d2d6e7e4944e5ee9994a2bf6fb86cb280d413abc
donrestarone <shashikejayatunge@gmail.com>
2021-05-17 19:45:49 -0400

    add rich text

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -2,6 +2,8 @@ class ForumPost < ApplicationRecord
   belongs_to :forum_thread, counter_cache: true, touch: true
   belongs_to :user
 
+  has_rich_text :body
+
   validates :user_id, :body, presence: true
 
   scope :sorted, -> { order(:created_at) }
